5d1ca74e069468be74d5ea3a9dbd9b9d390f2417
Phoneria <90638359+Phoneria@users.noreply.github.com>
2022-07-18 08:56:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,87 +3,50 @@ from datetime import datetime as dt
 import time
 from binance import Client
 import csv
-from datetime import datetime,timedelta
-import requests
+from datetime import datetime, timedelta
 import pandas_ta as ta
-
+import requests
 
 
 def message(bot_mesaj):
     id_list = ["1598072365"]
+ 
+ 
     for i in id_list:
-        bot_token= "5424258200:AAGNGZlN5HevI2fnyQedvD7v8XPsFFSBBJA"
-        url ="https://api.telegram.org/bot"+bot_token+"/sendMessage?chat_id="+i+"&parse_mode=Markdown&text="+bot_mesaj
-        response =requests.get(url)
+        bot_token = "5424258200:AAGNGZlN5HevI2fnyQedvD7v8XPsFFSBBJA"
+        url = "https://api.telegram.org/bot" + bot_token + "/sendMessage?chat_id=" + i + "&parse_mode=Markdown&text=" + bot_mesaj
+        response = requests.get(url)
 
 
+def tg_writer(coin_name):
+    client = Client(None, None)
 
-def tg_writer(coin_name,period):
-
-
-
-    client =Client(None,None)
-
-    def bring_data(symbol,periot,open,end):
-        candles=client.get_historical_klines(symbol,periot,open,end)
+    def bring_data(symbol, periot, open, end):
+        candles = client.get_historical_klines(symbol, periot, open, end)
         return candles
 
-    def create_csv(symbol,candles):
-        csv_file=open(symbol+period+"TG.csv","w",newline="")
-        writer=csv.writer(csv_file)
+    def create_csv(symbol, candles):
+        csv_file = open(symbol + "30MINTG.csv", "w", newline="")
+        writer = csv.writer(csv_file)
         for candle in candles:
             writer.writerow(candle)
 
         csv_file.close()
 
-    if period == "30MIN":
+    start_time = datetime.today() - timedelta(hours=50)
+    finish_time = datetime.today() + timedelta(days=1)
+    interval = Client.KLINE_INTERVAL_30MINUTE
 
-        start_time = datetime.today() - timedelta(hours=6)
-        finish_time = datetime.today() + timedelta(days=1)
-        interval = Client.KLINE_INTERVAL_30MINUTE
-
-    elif period == "1HOUR":
-
-        start_time = datetime.today() - timedelta(hours=9)
-        finish_time = datetime.today() + timedelta(days=1)
-        interval =  Client.KLINE_INTERVAL_1HOUR
-
-    elif period == "2HOUR":
-
-        start_time = datetime.today() - timedelta(hours=16)
-        finish_time = datetime.today() + timedelta(days=1)
-        interval = Client.KLINE_INTERVAL_2HOUR
-
-    elif period =="4HOUR":
-        start_time = datetime.today() - timedelta(hours=28)
-        finish_time = datetime.today() + timedelta(days=1)
-        interval = Client.KLINE_INTERVAL_4HOUR
-
-    elif period=="1DAY":
-
-        start_time = datetime.today() - timedelta(days=6)
-        finish_time = datetime.today() + timedelta(days=1)
-        interval = Client.KLINE_INTERVAL_1DAY
+    create_csv(coin_name, bring_data(coin_name, interval, str(start_time), str(finish_time)))
 
 
-
-
-    create_csv(coin_name,bring_data(coin_name, interval, str(start_time), str(finish_time)))
-
-
-def candle_stick(coin_name,period):
-
+def candle_stick(coin_name):
     titles = ["Open Time", "Open", "High", "Low", "Close", "Volume", "Close Time", "QAV", "NAT", "TBBAV", "TBQAV",
               "ignore"]
 
-    tg_writer(coin_name,period)
+    tg_writer(coin_name)
 
-    df = pd.read_csv(coin_name + period + "TG.csv", names=titles)
-
-
-
-
-
+    df = pd.read_csv(coin_name + "30MINTG.csv", names=titles)
 
     open_level = df["Open"]
     close_level = df["Close"]
@@ -94,173 +57,154 @@ def candle_stick(coin_name,period):
     def calculate_time(number):
         return dt.fromtimestamp(open_time.iloc[number] / 1000)
 
+    max_level = 0
+    min_level = 0
+    for i in range(5, len(close_level)):
 
 
-    for i in range(5,len(close_level)):
-        if True:
+        if calculate_time(i).hour == time.localtime().tm_hour and calculate_time(i).day==datetime.today().day:
+            if max_level[i] < high_level[i]:
+                message(
+                    coin_name + "\nSON 100 MUMUN EN YÜKSEK SEVİYESİ" + " \nZAMAN :  " + str(calculate_time(i).hour + 3) + " : " + str(
+                        calculate_time(i).minute))
 
+            if min_level[i] < low_level[i] :
+                message(
+                    coin_name + "\nSON 100 MUMUN EN DÜŞÜK SEVİYESİ" + " \nZAMAN :  " + str(
+                        calculate_time(i).hour + 3) + " : " + str(
+                        calculate_time(i).minute))
 
 
             # ENGULF CANDLE
-            if  (open_level[i] > close_level[i]) and (close_level[i - 1] > open_level[i - 1]) and (
-                    abs(close_level[i] - open_level[i]) > abs(close_level[i-1] - open_level[i-1])):
-                message(coin_name + "\nSHORT ENGULF CANDLE")
-                message("STRENGTH : 2")
-                message(coin_name + " : " + str(calculate_time(i)) + " : " + period)
-
-
-            if (open_level[i] < close_level[i]) and (close_level[i - 1] <open_level[i - 1]) and (
+            if (open_level[i] > close_level[i]) and (close_level[i - 1] > open_level[i - 1]) and (
                     abs(close_level[i] - open_level[i]) > abs(close_level[i - 1] - open_level[i - 1])):
-                message(coin_name + "\nLONG ENGULF CANDLE")
-                message("STRENGTH : 2")
-                message(coin_name + " : " + str(calculate_time(i)) + " : " + period)
+                message(coin_name + "\nSHORT ENGULF MUMU" + " \nZAMAN :  " + str(calculate_time(i).hour + 3) + " : " + str(
+                    calculate_time(i).minute))
+
+            if (open_level[i] < close_level[i]) and (close_level[i - 1] < open_level[i - 1]) and (
+                    abs(close_level[i] - open_level[i]) > abs(close_level[i - 1] - open_level[i - 1])):
+                message(coin_name + "\nLONG ENGULF MUMU" + "\nZAMAN : " + str(calculate_time(i).hour + 3) + " : " + str(
+                    calculate_time(i).minute))
+
             # GREEN CANDLE WITHOUT BOT SHADOW
             if low_level[i] == open_level[i]:
-                message(coin_name + "\nGREEN CANDLE WITHOUT BOT SHADOW ")
-                message("STRENGTH : 4")
-                message(coin_name + " : " + str(calculate_time(i)) + " : " + period)
+                message(coin_name + "\nALT İĞNESİZ YEŞİL MUM " + " \nZAMAN :  " + str(calculate_time(i).hour + 3) + " : " + str(
+                    calculate_time(i).minute))
+
             # RED CANDLE WITHOUT TOP SHADOW
             if high_level[i] == open_level[i]:
-                message(coin_name + "\nRED CANDLE WITHOUT TOP SHADOW")
-                message("STRENGTH : 4")
-                message(coin_name + " : " + str(calculate_time(i)) + " : " + period)
+                message(
+                    coin_name + "\nÜST İĞNESİZ KIRMIZI MUM" + " \nZAMAN :  " + str(calculate_time(i).hour + 3) + " : " + str(
+                        calculate_time(i).minute))
+
             # EVENING STAR
-            if (close_level[i] < open_level[i]) and ( abs(close_level[i]-open_level[i])> abs(close_level[i-1]-open_level[i-1])) and (
-                    abs(close_level[i-1] - open_level[i-1]) / (high_level[i]-low_level[i]) < 0.3) and (close_level[i-2]>open_level[i-2]) and(
-                    abs(close_level[i-2] - open_level[i-2]) >abs (close_level[i]-(open_level[i])))  and(
-                    abs(close_level[i-1] - open_level[i-1]) / (high_level[i-1]-low_level[i-1]) < 0.3) and (
-                    abs(close_level[i] - open_level[i]) / (high_level[i]-low_level[i]) > 0.3) and (
-                    abs(close_level[i-2] - open_level[i-2]) / (high_level[i-2]-low_level[i-2]) > 0.3)  :
-                message(coin_name + "\nEVENING STAR")
-                message("STRENGTH : 3")
-                message(coin_name + " : " + str(calculate_time(i)) + " : " + period)
+            if (close_level[i] < open_level[i]) and (
+                    abs(close_level[i] - open_level[i]) > abs(close_level[i - 1] - open_level[i - 1])) and (
+                    abs(close_level[i - 1] - open_level[i - 1]) / (high_level[i] - low_level[i]) < 0.3) and (
+                    close_level[i - 2] > open_level[i - 2]) and (
+                    abs(close_level[i - 2] - open_level[i - 2]) > abs(close_level[i] - (open_level[i]))) and (
+                    abs(close_level[i - 1] - open_level[i - 1]) / (high_level[i - 1] - low_level[i - 1]) < 0.3) and (
+                    abs(close_level[i] - open_level[i]) / (high_level[i] - low_level[i]) > 0.3) and (
+                    abs(close_level[i - 2] - open_level[i - 2]) / (high_level[i - 2] - low_level[i - 2]) > 0.3):
+                message(coin_name + "\nGECE YILDIZI" + " \nZAMAN :  " + str(calculate_time(i).hour + 3) + " : " + str(
+                    calculate_time(i).minute))
+
             # MORNING STAR
 
-            if (close_level[i] > open_level[i]) and ( abs(close_level[i]-open_level[i])> abs(close_level[i-1]-open_level[i-1])) and (close_level[i-2]<open_level[i-2]) and(
-                    abs(close_level[i-2] - open_level[i-2]) <abs (close_level[i]-(open_level[i]))) and (
-                    abs(close_level[i-1] - open_level[i-1]) / (high_level[i-1]-low_level[i-1]) < 0.3) and (
-                    abs(close_level[i] - open_level[i]) / (high_level[i]-low_level[i]) > 0.3) and (
-                    abs(close_level[i-2] - open_level[i-2]) / (high_level[i-2]-low_level[i-2]) > 0.3) :
-                message(coin_name + "\nMORNING STAR")
-                message("STRENGTH : 3")
-                message(coin_name + " : " + str(calculate_time(i)) + " : " + period)
+            if (close_level[i] > open_level[i]) and (
+                    abs(close_level[i] - open_level[i]) > abs(close_level[i - 1] - open_level[i - 1])) and (
+                    close_level[i - 2] < open_level[i - 2]) and (
+                    abs(close_level[i - 2] - open_level[i - 2]) < abs(close_level[i] - (open_level[i]))) and (
+                    abs(close_level[i - 1] - open_level[i - 1]) / (high_level[i - 1] - low_level[i - 1]) < 0.3) and (
+                    abs(close_level[i] - open_level[i]) / (high_level[i] - low_level[i]) > 0.3) and (
+                    abs(close_level[i - 2] - open_level[i - 2]) / (high_level[i - 2] - low_level[i - 2]) > 0.3):
+                message(coin_name + "\nSABAH YILDIZI" + " \nZAMAN :  " + str(calculate_time(i).hour + 3) + " : " + str(
+                    calculate_time(i).minute))
+
             if (close_level[i] < open_level[i]) and (close_level[i - 1] > open_level[i - 1]) \
                     and (close_level[i - 2] > open_level[i - 2]) and (close_level[i - 3] < open_level[i - 3]) and \
                     (close_level[i - 2] - open_level[i - 2] > open_level[i - 3] - close_level[i - 3]) and \
                     (open_level[i] - close_level[i] > close_level[i - 1] - open_level[i - 1]):
-                message(coin_name+ "\nDOUBLE ENGULF SHORT")
-                message("STRENGTH : 2")
-                message(coin_name + " : " + str(calculate_time(i)) + " : " + period)
+                message(coin_name + "\nİKİLİ ENGULF SHORT" + " \nZAMAN :  " + str(calculate_time(i).hour + 3) + " : " + str(
+                    calculate_time(i).minute))
+
             if (close_level[i] > open_level[i]) and (close_level[i - 1] < open_level[i - 1]) \
                     and (close_level[i - 2] < open_level[i - 2]) and (close_level[i - 3] > open_level[i - 3]) and \
                     (close_level[i - 2] - open_level[i - 2] < open_level[i - 3] - close_level[i - 3]) and \
                     (open_level[i] - close_level[i] < close_level[i - 1] - open_level[i - 1]):
-                message(coin_name+ "\nDOUBLE ENGULF LONG")
-                message("STRENGTH : 2")
-                message(coin_name + " : " + str(calculate_time(i)) + " : " + period)
+                message(coin_name + "\nİKİLİ ENGULF LONG" + "\nZAMAN :  " + str(calculate_time(i).hour + 3) + " : " + str(
+                    calculate_time(i).minute))
+
             if (close_level[i] > open_level[i]) and (close_level[i - 2] > open_level[i - 2]) and (
                     close_level[i - 1] < open_level[i - 1]) and (
                     close_level[i] - open_level[i] > open_level[i - 1] - close_level[i - 1]) and (
                     close_level[i - 2] - open_level[i - 2] > open_level[i - 1] - close_level[i - 1]):
+                message(coin_name + "\nSANDVİÇ LONG" + " \nZAMAN :  " + str(calculate_time(i).hour + 3) + " : " + str(
+                    calculate_time(i).minute))
 
-                message(coin_name + "\nTRIPLE CANDLE LONG")
-                message("STRENGTH : 3")
-                message(coin_name + " : " + str(calculate_time(i)) + " : " + period)
             if (close_level[i] < open_level[i]) and (close_level[i - 2] < open_level[i - 2]) and (
                     close_level[i - 1] > open_level[i - 1]) and (
                     open_level[i] - close_level[i] > close_level[i - 1] - open_level[i - 1]) and (
                     open_level[i - 2] - close_level[i - 2] > close_level[i - 1] - open_level[i - 1]):
-
-                message(coin_name + "\nTRIPLE CANDLE SHORT")
-                message("STRENGTH : 3")
-                message(coin_name + " : " + str(calculate_time(i)) + " : " + period)
-
-            if abs(close_level[i]-open_level[i])/(high_level[i]-low_level[i]) < 0.382:
-                message(coin_name + "\nDOJI CANDLE")
-                message(coin_name+" : "+str(calculate_time(i))+" : "+period)
+                message(coin_name + "\nSANDVİÇ SHORT" + " \nZAMAN :  " + str(calculate_time(i).hour + 3) + " : " + str(
+                    calculate_time(i).minute))
 
             if (close_level[i] > open_level[i]) and (close_level[i - 4] > open_level[i - 4]) and (
                     close_level[i - 1] < open_level[i - 1]) and (close_level[i - 2] < open_level[i - 2]) and (
                     close_level[i - 3] < open_level[i - 3]) \
                     and (open_level[i - 4] < open_level[i]):
-                message(coin_name + "\nTHREE CANDLE LONG ")
-                message(coin_name + " : " + str(calculate_time(i)) + " : " + period)
-                message("STRENGTH : 3")
+                message(coin_name + "\nÜÇLÜ ENGULF LONG " + " \nZAMAN :  " + str(calculate_time(i).hour + 3) + " : " + str(
+                    calculate_time(i).minute))
 
             if (close_level[i] < open_level[i]) and (close_level[i - 4] < open_level[i - 4]) and (
                     close_level[i - 1] > open_level[i - 1]) and (close_level[i - 2] > open_level[i - 2]) and (
                     close_level[i - 3] > open_level[i - 3]) \
                     and (open_level[i - 4] > open_level[i]):
-                message(coin_name + "\nTHREE CANDLE SHORT ")
-                message(coin_name + " : " + str(calculate_time(i)) + " : " + period)
-                message("STRENGTH : 3")
+                message(coin_name + "\nÜÇLÜ ENGULF SHORT " + " \nZAMAN :  " + str(calculate_time(i).hour + 3) + " : " + str(
+                    calculate_time(i).minute))
 
+            if (close_level[i] < open_level[i]) and (close_level[i - 1] < open_level[i - 1]) and (
+                    close_level[i - 2] < open_level[i - 2]) and \
+                    close_level[i] < low_level[i - 1] and close_level[i - 1] < low_level[i - 2]:
+                message(coin_name + "\nÜÇ KARA KARGA" + "\nZAMAN :  " + str(calculate_time(i).hour + 3) + " : " + str(
+                    calculate_time(i).minute))
 
-            if (close_level[i]<open_level[i]) and (close_level[i-1]<open_level[i-1]) and (close_level[i-2]<open_level[i-2]) and \
-                close_level[i] < low_level[i-1] and close_level[i-1] < low_level[i-2]:
-
-                message(coin_name + "\nTHREE BLACK CROWN")
-                message(coin_name + " : " + str(calculate_time(i)) + " : " + period)
-                message("STRENGTH : 2")
-
-            if close_level[i] > open_level[i] and close_level[i - 1] > open_level[i - 1] and close_level[i - 2] > open_level[i - 2] and \
+            if close_level[i] > open_level[i] and close_level[i - 1] > open_level[i - 1] and close_level[i - 2] > \
+                    open_level[i - 2] and \
                     close_level[i] > high_level[i - 1] and close_level[i - 1] > high_level[i - 2]:
-                message(coin_name + "\nTHREE WHITE SOLDIER ")
-                message(coin_name + " : " + str(calculate_time(i)) + " : " + period)
-                message("STRENGTH : 2")
+                message(coin_name + "\nÜÇ BEYAZ ASKER " + " \nZAMAN :  " + str(calculate_time(i).hour + 3) + " : " + str(
+                    calculate_time(i).minute))
 
             if close_level[i] < open_level[i] and \
                     close_level[i - 1] < open_level[i - 1] and \
                     close_level[i - 2] < open_level[i - 2] and \
                     close_level[i - 3] < open_level[i - 3] and \
                     close_level[i - 4] < open_level[i - 4]:
-                message(coin_name + "\nFIVE CANDLE SHORT ")
-                message(coin_name + " : " + str(calculate_time(i)) + " : " + period)
-                message("STRENGTH : 2")
+                message(coin_name + "\nBEŞ MUM SHORT " + " \nZAMAN :  " + str(calculate_time(i).hour + 3) + " : " + str(
+                    calculate_time(i).minute))
+
             if close_level[i] > open_level[i] and \
                     close_level[i - 1] > open_level[i - 1] and \
                     close_level[i - 2] > open_level[i - 2] and \
                     close_level[i - 3] > open_level[i - 3] and \
                     close_level[i - 4] > open_level[i - 4]:
-
-                message(coin_name + "\nFIVE CANDLE LONG ")
-                message(coin_name + " : " + str(calculate_time(i)) + " : " + period)
-                message("STRENGTH : 2")
+                message(coin_name + "\nBEŞ MUM LONG " + " \nZAMAN :  " + str(calculate_time(i).hour + 3) + " : " + str(
+                    calculate_time(i).minute ))
 
 
 
+        else:
+            if high_level[i] >= max_level:
+                max_level = high_level[i]
+            if low_level[i] <= min_level:
+                min_level = low_level[i]
 
 
 
-while True  :
-    if (time.localtime().tm_min==29 or time.localtime().tm_min == 59 ) and time.localtime().tm_sec == 0:
-        message("NEW MESSAGE!!!!\n30MIN")
-        candle_stick("NEARUSDT","30MIN")
-        candle_stick("BTCUSDT","30MIN")
-        candle_stick("ETHUSDT","30MIN")
 
-    if  time.localtime().tm_min == 59 and time.localtime().tm_sec == 10:
-        message("NEW MESSAGE!!!!\n1HOUR")
-        candle_stick("NEARUSDT", "1HOUR")
-        candle_stick("BTCUSDT", "1HOUR")
-        candle_stick("ETHUSDT", "1HOUR")
-
-    if  time.localtime().tm_hour%2 == 1 and time.localtime().tm_min == 59 and time.localtime().tm_sec == 20:
-        message("NEW MESSAGE!!!!\n2HOUR")
-        candle_stick("NEARUSDT", "2HOUR")
-        candle_stick("BTCUSDT", "2HOUR")
-        candle_stick("ETHUSDT", "2HOUR")
+message("YENİDEN BAŞLADI")
+candle_stick("NEARUSDT")
+candle_stick("BTCUSDT")
 
 
-    if  time.localtime().tm_hour%4 == 2 and time.localtime().tm_min == 59 and time.localtime().tm_sec == 30:
-        message("NEW MESSAGE!!!!\n4HOUR")
-        candle_stick("NEARUSDT", "4HOUR")
-        candle_stick("BTCUSDT", "4HOUR")
-        candle_stick("ETHUSDT", "4HOUR")
 
-
-    if  time.localtime().tm_hour%24 == 2 and time.localtime().tm_min == 59 and time.localtime().tm_sec == 40:
-        message("NEW MESSAGE!!!!\n1DAY")
-        candle_stick("NEARUSDT", "1DAY")
-        candle_stick("BTCUSDT", "1DAY")
-        candle_stick("ETHUSDT", "1DAY")
